468e0cc13600b2b7be365614108644e82c6c6c47
Adham Magdy <adhammagdy.dev@gmail.com>
2024-02-10 23:55:36 +0200

    Add pagination functionality to PDFRenderer component

--- src/components/PDFRenderer.tsx
+++ src/components/PDFRenderer.tsx
@@ -1,19 +1,79 @@
 "use client";
-import { Loader2 } from "lucide-react";
+import { ChevronDown, ChevronLeft, ChevronUp, Loader2 } from "lucide-react";
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import { toast } from "@/components/ui/use-toast";
 import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { unknown } from "zod";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDFRenderer = ({ url }: { url: string }) => {
   const { width, ref } = useResizeDetector();
+  const [numPages, setNumPages] = React.useState<number>();
+  const [curPage, setCurPage] = React.useState<number>(1);
+  const pageInputRef = React.useRef<HTMLInputElement>(null);
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 flex items-center justify-between w-full border-zinc-200 border-b px-2">
-        <div className="flex items-center gap-1,5">header</div>
+        <div className="flex items-center gap-1,5">
+          <Button
+            disabled={curPage === 1}
+            variant={"ghost"}
+            aria-label="previous page"
+            onClick={() => {
+              if (curPage > 1) {
+                setCurPage((prev) => prev - 1);
+                pageInputRef.current!.value = String(curPage - 1);
+              }
+            }}
+          >
+            <ChevronUp className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5">
+            <Input
+              className="w-12 h-8"
+              accept="number"
+              ref={pageInputRef}
+              onInput={({ currentTarget }) => {
+                console.log("changing page");
+                const pageNumber = Number(currentTarget.value);
+                if (pageNumber > 0 && pageNumber <= numPages!) {
+                  setCurPage(pageNumber);
+                } else if (
+                  currentTarget.value === "" ||
+                  currentTarget.value === "0"
+                ) {
+                  console.log("empty page number");
+                } else {
+                  console.log("invalid page number:", pageNumber);
+                  if (currentTarget.value.length > 0)
+                    currentTarget.value = currentTarget.value.slice(0, -1);
+                }
+              }}
+            />
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+          <Button
+            disabled={numPages === undefined || curPage === numPages!}
+            variant={"ghost"}
+            aria-label="next page"
+            onClick={() => {
+              if (curPage < numPages!) {
+                setCurPage((prev) => prev + 1);
+                pageInputRef.current!.value = String(curPage + 1);
+              }
+            }}
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </div>
       </div>
       <div className="flex-1 w-full max-h-screen">
         <div ref={ref}>
@@ -23,6 +83,9 @@ const PDFRenderer = ({ url }: { url: string }) => {
                 <Loader2 className="my-24 h-6 w-6 animate-spin" />
               </div>
             }
+            onLoadSuccess={({ numPages }) => {
+              setNumPages(numPages);
+            }}
             onLoadError={() => {
               toast({
                 title: "Something went wrong!",
@@ -33,7 +96,15 @@ const PDFRenderer = ({ url }: { url: string }) => {
             file={url}
             className={"max-h-full"}
           >
-            <Page width={width ?? 1} pageNumber={1} />
+            <Page
+              loading={
+                <div className="flex justify-center">
+                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                </div>
+              }
+              width={width ?? 1}
+              pageNumber={curPage}
+            />
           </Document>
         </div>
       </div>
