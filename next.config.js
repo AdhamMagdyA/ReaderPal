08dfa9d3e966aa084629582ebe7260b642db422d
Adham Magdy <adhammagdy.dev@gmail.com>
2024-02-28 17:24:53 +0200

    indexing pdf after upload is success

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
+    config.module.rules.push({
+      test: /\.node$/,
+      exclude: /node_modules/,
+      use: "ignore-loader",
+    });
     return config;
   },
 };
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,6 +2,12 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { pinecone } from "@/lib/pinecone";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { HuggingFaceInferenceEmbeddings } from "langchain/embeddings/hf";
+import { PineconeStore } from "@langchain/pinecone";
 
 const f = createUploadthing();
 
@@ -28,8 +34,58 @@ export const ourFileRouter = {
           status: "PROCESSING",
         },
       });
-      if (!createdFile) {
-        throw new UploadThingError("Failed to create file");
+      try {
+        const response = await fetch(createdFile.url);
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+        const pagesAmt = pageLevelDocs.length;
+
+        const pineconeIndex = pinecone.Index("reader-pal");
+
+        let embeddings;
+        if (process.env.OPENAI_API_KEY) {
+          embeddings = new OpenAIEmbeddings({
+            openAIApiKey: process.env.OPENAI_API_KEY,
+          });
+        } else if (process.env.MISTRAL_API_KEY) {
+          embeddings = new MistralAIEmbeddings({
+            apiKey: process.env.MISTRAL_API_KEY,
+          });
+        } else {
+          embeddings = new HuggingFaceInferenceEmbeddings({
+            maxRetries: 3,
+            onFailedAttempt: (error) => {
+              console.log(
+                `Failed to embed page ${error.attemptNumber} times. Retrying...`
+              );
+            },
+          });
+        }
+        console.log("Embeddings: ", embeddings);
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+        console.log("Indexed successfully");
+        // file is uploaded and indexed successfully
+        await db.file.update({
+          where: { id: createdFile.id },
+          data: {
+            status: "SUCCESS",
+          },
+        });
+      } catch (e) {
+        // failed to index the file
+        console.log("an error occured while indexing the file: ", e);
+
+        await db.file.update({
+          where: { id: createdFile.id },
+          data: {
+            status: "FAILED",
+          },
+        });
       }
     }),
 } satisfies FileRouter;
